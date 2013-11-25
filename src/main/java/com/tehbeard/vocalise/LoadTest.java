dfbb928e79165b331a969d2bc15d2b997ef35e6b
James <beardyjames@live.co.uk>
2013-11-25 19:53:47 +0000

    Test loading class

---
+++ src/main/java/com/tehbeard/vocalise/LoadTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tehbeard.vocalise;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author James
+ */
+public class LoadTest {
+    public static void main(String[] args) throws FileNotFoundException{
+        PromptGraph graph = new PromptGraphBuilder().makeGraph(new FileInputStream(new File("C:\\Users\\James\\Desktop\\test.json")));
+        System.out.println("::Loading graph::");
+        System.out.println(graph.getInitialPrompt());
+    }
+}
